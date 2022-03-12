8ac664de290f160c5ab94ac758cb6724b089291e
Mohamed Shibl <31707409+mohamdshibl@users.noreply.github.com>
2022-03-12 22:52:31 +0200

    day1

---
+++ Storak/Views/Storyboards/Shibl/MyAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyAccountViewController.swift
+//  Storak
+//
+//  Created by Mohamed Shibl on 11/03/2022.
+//
+
+import UIKit
+
+class MyAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func ToSignInVC(_ sender: Any) {
+        let signinVC = (storyboard?.instantiateViewController(withIdentifier: "signinVC")) as! LogInViewController
+        navigationController?.pushViewController(signinVC, animated: true)
+    }
+    
+    @IBAction func ToSignUpVC(_ sender: Any) {
+        let signupVC = (storyboard?.instantiateViewController(withIdentifier: "signupVC")) as! SignUpViewController
+        navigationController?.pushViewController(signupVC, animated: true)
+        
+    }
+    
+}
---
+++ Storak/Views/Storyboards/Shibl/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  Storak
+//
+//  Created by Mohamed Shibl on 11/03/2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signUp(_ sender: Any) {
+    }
+    
+    @IBAction func returnToSignIn(_ sender: Any) {
+    }
+}
