176dd6439d0f5bb5d8249e1dc37a87c01066abf6
Eyad Heikal <eyadheikal@gmail.com>
2022-03-13 18:21:20 +0200

    change root viewController

--- Storak/SceneDelegate.swift
+++ Storak/SceneDelegate.swift
@@ -16,7 +16,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: windowScene)
+
+        class CustomViewController: UIViewController {
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                view.backgroundColor = .red
+            }
+        }
+
+        let viewController = CustomViewController()
+        window?.rootViewController = UINavigationController(rootViewController: viewController)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
