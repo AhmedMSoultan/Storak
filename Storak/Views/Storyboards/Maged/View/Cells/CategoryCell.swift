98407efb4a300ff26ecba59d1bc961ac6909d740
Mohamed Maged <mohamedmaged.live@gmail.com>
2022-03-17 02:43:43 +0200

    last edit for constrains

--- Storak/Views/Storyboards/Maged/View/Cells/CategoryCell.swift
+++ Storak/Views/Storyboards/Maged/View/Cells/CategoryCell.swift
@@ -20,10 +20,12 @@ class CategoryCell: UITableViewCell {
     }
 
     func addShadowToCategoryImageView() {
-        imageContainer.layer.shadowColor = UIColor.gray.cgColor.copy(alpha: 0.6)
-        imageContainer.layer.shadowOpacity = 1
+        imageContainer.layer.backgroundColor = UIColor.white.cgColor
+        imageContainer.layer.shadowColor = UIColor.black.cgColor
+        imageContainer.layer.shadowOpacity = 0.1
+        imageContainer.layer.masksToBounds = false
         imageContainer.layer.shadowOffset = .zero
-        imageContainer.layer.shadowRadius = 10
+        imageContainer.layer.shadowRadius = 7
         imageContainer.layer.cornerRadius = 5
     }
   
