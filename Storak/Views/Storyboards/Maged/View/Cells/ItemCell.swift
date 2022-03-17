e029fa1204752bd4a7285240d516532f751fe699
Mohamed Maged <mohamedmaged.live@gmail.com>
2022-03-17 03:07:44 +0200

    create item cell

---
+++ Storak/Views/Storyboards/Maged/View/Cells/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Storak
+//
+//  Created by Mohamed Maged on 17/03/2022.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
